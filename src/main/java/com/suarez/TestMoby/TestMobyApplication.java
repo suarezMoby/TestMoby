889616e7f0977ea19a0957f12db7b4324ad7ac2a
Danilo Suarez <danilomdq@gmail.com>
2021-09-26 15:39:07 -0300

    Iniciodel proyecto

---
+++ src/main/java/com/suarez/TestMoby/TestMobyApplication.java
@@ -0,0 +1,13 @@
+package com.suarez.TestMoby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMobyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestMobyApplication.class, args);
+	}
+
+}
