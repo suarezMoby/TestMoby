0a3a6cef18b9cbe4f272dbb3592f186339ea89b4
suarez <dsuarez@mobydigital.com>
2021-10-03 12:15:40 -0300

    cambios en candidatoRepository y CandidatoServiceImplement

--- src/main/java/com/suarez/TestMoby/repository/CandidatoRepository.java
+++ src/main/java/com/suarez/TestMoby/repository/CandidatoRepository.java
@@ -1,9 +1,12 @@
 package com.suarez.TestMoby.repository;
 
 import com.suarez.TestMoby.model.entitys.Candidato;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CandidatoRepository extends CrudRepository<Candidato, Integer> {
+
+
 }
--- src/main/java/com/suarez/TestMoby/services/implement/CandidatoServiceImplement.java
+++ src/main/java/com/suarez/TestMoby/services/implement/CandidatoServiceImplement.java
@@ -1,10 +1,14 @@
 package com.suarez.TestMoby.services.implement;
 
 import com.suarez.TestMoby.model.entitys.Candidato;
+import com.suarez.TestMoby.repository.CandidatoRepository;
 import com.suarez.TestMoby.services.CandidatoService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class CandidatoServiceImplement implements CandidatoService {
 
+    @Autowired
+    private CandidatoRepository candidatoRepository;
 
     @Override
     public Candidato findByTecnologia(String tecnologia) {
