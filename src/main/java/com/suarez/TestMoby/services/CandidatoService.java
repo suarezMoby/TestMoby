ce8e9acc880594209b7a161d19e934ac2c7aeb18
suarez <dsuarez@mobydigital.com>
2021-10-03 11:19:11 -0300

    cree la funcion findByTecnologia

--- src/main/java/com/suarez/TestMoby/services/CandidatoService.java
+++ src/main/java/com/suarez/TestMoby/services/CandidatoService.java
@@ -1,4 +1,8 @@
 package com.suarez.TestMoby.services;
 
+import com.suarez.TestMoby.model.entitys.Candidato;
+
 public interface CandidatoService {
+
+    Candidato findByTecnologia(String tecnologia);
 }
--- src/main/java/com/suarez/TestMoby/services/implement/CandidatoServiceImplement.java
+++ src/main/java/com/suarez/TestMoby/services/implement/CandidatoServiceImplement.java
@@ -1,6 +1,13 @@
 package com.suarez.TestMoby.services.implement;
 
+import com.suarez.TestMoby.model.entitys.Candidato;
 import com.suarez.TestMoby.services.CandidatoService;
 
 public class CandidatoServiceImplement implements CandidatoService {
+
+
+    @Override
+    public Candidato findByTecnologia(String tecnologia) {
+        return null;
+    }
 }
