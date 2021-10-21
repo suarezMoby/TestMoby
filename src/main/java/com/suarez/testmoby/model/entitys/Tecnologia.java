f099bbf9c9f25f061fbfdcb050bf95e888b18a8a
suarez <dsuarez@mobydigital.com>
2021-10-21 08:50:47 -0300

    correccion de algunas cosas de sonar

--- src/main/java/com/suarez/testmoby/model/entitys/Tecnologia.java
+++ src/main/java/com/suarez/testmoby/model/entitys/Tecnologia.java
@@ -11,6 +11,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.io.Serializable;
 
 @Entity
 @Data
@@ -18,7 +19,7 @@ import javax.persistence.Table;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "tecnologia")
-public class Tecnologia {
+public class Tecnologia implements Serializable {
 
    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
