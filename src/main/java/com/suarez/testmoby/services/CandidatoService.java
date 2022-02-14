db6f028a160955a9fa5134fd84477bfc51ae7e3c
suarez <dsuarez@mobydigital.com>
2022-02-14 20:14:50 -0300

    se modifico la cabecera d ela funcion

--- src/main/java/com/suarez/testmoby/services/CandidatoService.java
+++ src/main/java/com/suarez/testmoby/services/CandidatoService.java
@@ -3,11 +3,12 @@ package com.suarez.testmoby.services;
 import com.suarez.testmoby.model.entitys.Candidato;
 import com.suarez.testmoby.model.views.CandidatoDto;
 
+import java.text.ParseException;
 import java.util.List;
 
 public interface CandidatoService {
 
-    public Candidato createCandidato(Candidato candidato);
+    public CandidatoDto guardar(CandidatoDto candidatoDto) throws ParseException;
 
     Candidato actualizarEstadoDeCandidato(Integer id);
 
