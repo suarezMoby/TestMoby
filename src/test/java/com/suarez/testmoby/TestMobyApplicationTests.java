610cb0f74e848332593adfd95aa17c8777d04b99
suarez <dsuarez@mobydigital.com>
2022-02-15 16:19:27 -0300

    Limpieza de codigo

--- src/test/java/com/suarez/testmoby/TestMobyApplicationTests.java
+++ src/test/java/com/suarez/testmoby/TestMobyApplicationTests.java
@@ -1,13 +1,9 @@
 package com.suarez.testmoby;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TestMobyApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
